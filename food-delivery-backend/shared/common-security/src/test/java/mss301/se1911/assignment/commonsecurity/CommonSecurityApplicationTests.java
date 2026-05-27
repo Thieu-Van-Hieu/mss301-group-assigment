99ddce301bd3f32c76e8aa3d353a35b3e4a0a572
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-05-27 14:26:28 +0700

    feat: khởi tạo cấu trúc dự án

---
+++ food-delivery-backend/shared/common-security/src/test/java/mss301/se1911/assignment/commonsecurity/CommonSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package mss301.se1911.group.assignment.commonsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
