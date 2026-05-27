1328aeed464e028d10efd4e29fbe64c9b5ad145f
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-05-27 14:26:28 +0700

    feat: khởi tạo cấu trúc dự án

---
+++ food-delivery-backend/shared/common-events/src/test/java/mss301/se1911/assignment/commonevents/CommonEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package mss301.se1911.group.assignment.commonevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ food-delivery-backend/shared/common-web/src/test/java/mss301/se1911/assignment/commonweb/CommonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package mss301.se1911.group.assignment.commonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
