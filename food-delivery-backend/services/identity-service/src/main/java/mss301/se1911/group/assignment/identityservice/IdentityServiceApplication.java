a88eea657d977449809edeaee5999bb1a890f857
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-06-14 18:13:00 +0700

    feat(identity): thiết lập feign client để giao tiếp với keycloak

--- food-delivery-backend/services/identity-service/src/main/java/mss301/se1911/group/assignment/identityservice/IdentityServiceApplication.java
+++ food-delivery-backend/services/identity-service/src/main/java/mss301/se1911/group/assignment/identityservice/IdentityServiceApplication.java
@@ -2,8 +2,10 @@ package mss301.se1911.group.assignment.identityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class IdentityServiceApplication {
 
     public static void main(String[] args) {
