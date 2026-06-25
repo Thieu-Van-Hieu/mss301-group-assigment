4e3ab9c6f8bc7553e94e8d21afc88d92536219ea
Mr.NoBody <thieuefvanwhieues@gmail.com>
2026-06-25 17:07:31 +0700

    feat(common-constants): tạo common-constants để chia sẽ chung các const

---
+++ food-delivery-backend/shared/common-constants/src/main/java/mss301/se1911/group/assignment/commonconstants/utils/GatewayConstraints.java
@@ -0,0 +1,17 @@
+package mss301.se1911.group.assignment.commonconstants.utils;
+
+public class GatewayConstraints {
+    public static final String HEADER_CORRELATION_ID = "X-Correlation-Id";
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String HEADER_USER_ID = "X-User-ID";
+    public static final String HEADER_EMAIL = "X-Email";
+    public static final String HEADER_ROLE = "X-Role";
+    public static final String HEADER_FULL_NAME = "X-User-FullName";
+    public static final String HEADER_PHONE = "X-User-Phone";
+    public static final String HEADER_ACTIVE = "X-User-Active";
+    public static final String HEADER_ENABLED = "X-User-Enabled";
+
+    public static final int ORDER_CORRELATION_ID = -200;
+    public static final int ORDER_LOGGING_ID = -100;
+    public static final int ORDER_AUTHORIZATION_ID = -90;
+}
